b271362d83c7b83f8155b1779f9f470477258ee2
William Santos <Willsantos.edf@gmail.com>
2023-01-30 14:47:46 -0300

    Implementando validações.

--- cliente/serializers.py
+++ cliente/serializers.py
@@ -9,9 +9,11 @@ class ClienteSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf': "O CPF deve ter 11 dígitos"})
+            raise serializers.ValidationError({'cpf': "O CPF inválido"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome': "Não é permitido caracteres numéricos na variável nome!"})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg': "O RG deve ter 9 dígitos!"})
+        if not celular_valido(data['celular']):
+             raise serializers.ValidationError({'celular': "O numero de celular deve seguir este modelo: 71 91234-5678"})
         return data
--- cliente/validators.py
+++ cliente/validators.py
@@ -1,18 +1,21 @@
+import re
+from validate_docbr import CPF
 
 def cpf_valido(numero_cpf):
-    return len(numero_cpf) == 11
+    cpf = CPF()
+    return cpf.validate(numero_cpf)
         
 def nome_valido(nome):
     return nome.isalpha()
   
-
 def rg_valido(numero_rg):
     return len(numero_rg) == 9
        
-
-
-def validate_celular(self, celular):
-    if len(celular) < 11:
-        raise serializers.ValidationError("O celular deve ter, no mínimo, 11 dígitos!")
-    return celular
+def celular_valido(numero_celular):
+    """Verifica se celular é válido"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta =re.findall(modelo, numero_celular)
+    return resposta
+   
+        
  
